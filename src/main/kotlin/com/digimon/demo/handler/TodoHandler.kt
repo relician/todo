a40d7ed7be25e5c3f3fb4d23b1e8264e77132759
digimon <digimon.1740@gmail.com>
2019-08-28 15:56:05 +0900

    handler 수정

--- src/main/kotlin/com/digimon/demo/handler/TodoHandler.kt
+++ src/main/kotlin/com/digimon/demo/handler/TodoHandler.kt
@@ -4,10 +4,10 @@ import com.digimon.demo.domain.todo.Todo
 import com.digimon.demo.domain.todo.TodoRepository
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.BodyInserters.fromObject
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.body
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.time.LocalDateTime
 import java.util.*
@@ -17,7 +17,7 @@ import java.util.*
 class TodoHandler(private val repo: TodoRepository) {
 
     fun getAll(req: ServerRequest) = ServerResponse.ok()
-            .body<List<Todo>>(Mono.justOrEmpty(repo.findAll()))
+            .body<List<Todo>>(Flux.just(repo.findAll()))
 
     fun getById(req: ServerRequest): Mono<ServerResponse> {
         val id = req.pathVariable("id").toLong()
@@ -25,32 +25,41 @@ class TodoHandler(private val repo: TodoRepository) {
     }
 
     fun save(req: ServerRequest): Mono<ServerResponse> {
-        return req
-                .bodyToMono(Todo::class.java)
-                .flatMap { todo -> Mono.fromCallable { repo.save(todo) } }
-                .flatMap { todo -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(fromObject(todo)) }
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(req.bodyToMono(Todo::class.java)
+                        .switchIfEmpty(Mono.empty())
+                        .filter(Objects::nonNull)
+                        .flatMap { todo -> Mono.fromCallable { repo.save(todo) }.then(Mono.just(todo)) }
+                )
     }
 
     fun done(req: ServerRequest): Mono<ServerResponse> {
         val id = req.pathVariable("id").toLong()
-        return Mono.justOrEmpty(repo.findById(id))
-                .filter(Objects::nonNull)
-                .flatMap { todo ->
-                    Mono.fromCallable {
-                        todo.done = true
-                        todo.modifiedAt = LocalDateTime.now()
-                        repo.save(todo)
-                    }
-                }
-                .flatMap { ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(Mono.justOrEmpty(repo.findAll())) }
+
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.justOrEmpty(repo.findById(id))
+                        .switchIfEmpty(Mono.empty())
+                        .filter(Objects::nonNull)
+                        .flatMap { todo ->
+                            Mono.fromCallable {
+                                todo.done = true
+                                todo.modifiedAt = LocalDateTime.now()
+                                repo.save(todo)
+                            }.then(Mono.just(todo))
+                        }
+                )
     }
 
     fun delete(req: ServerRequest): Mono<ServerResponse> {
         val id = req.pathVariable("id").toLong()
-        return Mono.justOrEmpty(repo.findById(id))
-                .filter(Objects::nonNull)
-                .flatMap { todo -> Mono.fromCallable { repo.delete(todo) } }
-                .flatMap { ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(Mono.justOrEmpty(repo.findAll())) }
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.justOrEmpty(repo.findById(id))
+                        .switchIfEmpty(Mono.empty())
+                        .filter(Objects::nonNull)
+                        .flatMap { todo -> Mono.fromCallable { repo.delete(todo) }.then(Mono.just(todo)) })
     }
 
 
